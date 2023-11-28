04352368dcb6170cbf45a464a7da74c1f6cf9ccc
Faisal Islam <faisy@hotmail.ca>
2023-11-28 00:51:15 -0700

    created create user function

---
+++ app.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import streamlit as st
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "fake",
+    database = "TPMS"
+)
+
+mycursor = mydb.cursor()
+print("Connection Established")
+
+def main():
+    st.title("Therapist Patient Matching System")
+
+    option = st.sidebar.selectbox("Select an operation",("Create User", "Test2"))
+
+    if option == "Create User":
+        st.subheader = "Create a User"
+        username = st.text_input("Enter your username")
+        password = st.text_input("Enter your password")
+        email = st.text_input("Enter your email")
+        type = st.radio("Select user type",("Therapist", "Patient"))
+
+        if type == "Therapist":
+            utype = "Therapist"
+        if type == "Patient":
+            utype = "Patient"
+
+        if st.button("Create"):
+            sql = "INSERT INTO Users(Username, Password, UserType, Email) values(%s, %s, %s, %s)"
+            val = (username, password, utype, email)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            st.success("User created successfully!", icon = "😄")
+
+if __name__ == "__main__":
+    main()
