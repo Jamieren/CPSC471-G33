fb4321e7fc1765427fddcb7777c5bbd489c8e262
Jamieren <jamie.ren16@gmail.com>
2023-12-10 23:56:34 -0700

    modify session 30%
    still can't update the database

--- create_session.py
+++ create_session.py
@@ -109,57 +109,40 @@ def create_session(mycursor, patient_id, mydb):
 def modify_session(mycursor, session_id, mydb):
     st.subheader("Modify Your Session")
 
-    # Fetch the current session details
-    mycursor.execute("SELECT SessionDate, SessionTime FROM Sessions WHERE SessionID = %s", (session_id,))
-    session_details = mycursor.fetchone()
+    # Allow the user to pick a new date
+    new_date = st.date_input("Select the new date for your session", min_value=datetime.date.today())
 
-    if session_details:
-        current_date, session_time = session_details
-        
-        # Convert session_time to a datetime.time object if it's not already one
-        if isinstance(session_time, datetime.timedelta):
-            # Convert timedelta to a total number of seconds
-            seconds = session_time.total_seconds()
-            # Convert seconds to a time object
-            current_time = (datetime.datetime.min + session_time).time()
+    # Query the database for the selected date's slots, excluding the current session
+    mycursor.execute(
+        "SELECT SessionTime, IsBooked FROM Sessions WHERE SessionDate = %s AND SessionID <> %s",
+        (new_date, session_id)
+    )
+    day_slots = mycursor.fetchall()
+
+    # Convert day_slots to the correct format
+    day_slots_dict = {}
+    for slot, booked in day_slots:
+        if isinstance(slot, datetime.time):  # Check if slot is a time object
+            slot_str = slot.strftime("%H:%M:%S")
         else:
-            current_time = session_time
+            slot_str = str(slot)  # Handle timedelta or other types
+        day_slots_dict[slot_str] = booked
 
-        st.write(f"Current session date: {current_date}")
-        st.write(f"Current session time: {current_time.strftime('%I:%M %p')}")
-
-        # Allow the user to pick a new date
-        new_date = st.date_input("Select the new date for your session", value=current_date, min_value=datetime.date.today())
-
-        # Query the database for the selected date's slots, excluding the current session
-        mycursor.execute(
-            "SELECT SessionTime FROM Sessions WHERE SessionDate = %s AND SessionID <> %s AND IsBooked = 1", 
-            (new_date, session_id)
-        )
-        # We only need the times of the booked slots to compare
-        booked_slots = [time.strftime("%H:%M:%S") for (time,) in mycursor.fetchall()]
-
-        # Display all slots, marking those unavailable if already booked
-        st.write("Please select a new time slot:")
-        current_datetime = datetime.datetime.now()
-        for slot in AVAILABLE_SLOTS:
-            slot_time = datetime.datetime.strptime(slot, "%H:%M:%S").time()
-            slot_datetime = datetime.datetime.combine(new_date, slot_time)
-            slot_str = slot_time.strftime("%I:%M %p")
-
-            # Check if the slot is in the future and not booked
-            if slot_datetime < current_datetime or slot in booked_slots:
-                st.write(f"{slot_str} - Not Available")
-            else:
-                if st.button(f"Change to {slot_str}"):
-                    # Update the session with the new date and time
-                    update_query = "UPDATE Sessions SET SessionDate = %s, SessionTime = %s WHERE SessionID = %s"
-                    mycursor.execute(update_query, (new_date, slot_time, session_id))
-                    mydb.commit()
-                    st.success(f"Your session has been changed to {new_date} at {slot_str}.")
-                    break  # Exit after the change to prevent multiple changes
-    else:
-        st.error("Session details not found.")
+    # Display all slots, marking those unavailable if already booked
+    st.write("Please select a new time slot:")
+    for slot in AVAILABLE_SLOTS:
+        slot_time = datetime.datetime.strptime(slot, "%H:%M:%S").time()
+        slot_str = slot_time.strftime("%I%p-%I%p")
+        if day_slots_dict.get(slot, 0):  # If the slot is booked
+            st.write(f"{slot_str} - Not Available")
+        else:
+            if st.button(f"Change to {slot_str}"):
+                # Update the session with the new date and time
+                update_query = "UPDATE Sessions SET SessionDate = %s, SessionTime = %s WHERE SessionID = %s"
+                mycursor.execute(update_query, (new_date, slot_time, session_id))
+                mydb.commit()
+                st.success(f"Your session has been changed to {new_date} at {slot_str}.")
+                break  # Exit after the change to prevent multiple changes
 
 
         
